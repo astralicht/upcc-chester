c21cb90730b86bfbc9e7b71ec98a6792513fc97b
Julac Ontina <eneioarzew@gmail.com>
2022-10-02 15:03:31 +0800

    upload

--- controllers/CreateController.php
+++ controllers/CreateController.php
@@ -12,9 +12,9 @@ class CreateController {
     function cartItem($data) { return (new CreateModel)->cartItem($data); }
     function productType($data) { return (new CreateModel)->productType($data); }
     function product($data) { return (new CreateModel)->product($data); }
-    function productTags($data) { return (new CreateModel)->productTags($data); }
+    // function productTags($data) { return (new CreateModel)->productTags($data); }
     function order($data) { return (new CreateModel)->order($data); }
-    function history($data) { return (new CreateModel)->history($data); }
+    // function history($data) { return (new CreateModel)->history($data); }
     function adminNewUser($data) { return (new CreateModel)->adminNewUser($data); }
     function clientOrder($data) { return (new CreateModel)->clientOrder($data); }
     
