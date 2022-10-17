a4fc73a653f8eac1eadcd730d60659dedd26e701
Julac Ontina <julac_15@yahoo.com>
2022-10-17 21:00:34 +0800

    Finished update model unit tests.

--- a-unit-tests/unit-tests-updatemodel.php
+++ a-unit-tests/unit-tests-updatemodel.php
@@ -86,33 +86,85 @@ checkIfSuccess($task, $UpdateModel, "updateImageData", [
 ]);
 
 $task = "Update existing product";
-checkIfSuccess($task, $UpdateModel, "product", [
-    "" => "",
-]);
+checkIfSuccess($task, $UpdateModel, "product", json_encode([
+    "product-id" => "1",
+    "unit-price" => "50",
+    "name" => "Rubber banana 30 pcs.",
+    "material" => "Rubber",
+    "brand" => "Banana Republic",
+    "connection_type" => "",
+    "length" => "5cm",
+    "width" => "5cm",
+    "thickness" => ".3cm",
+    "type_id" => "9",
+    "image-input" => "",
+    "image-type" => "",
+    "old-image-path" => "",
+]));
 
 $task = "Update non-existent product";
-checkIfSuccess($task, $UpdateModel, "product", [
-    "" => "",
-]);
+checkIfSuccess($task, $UpdateModel, "product", json_encode([
+    "product-id" => "2",
+    "unit-price" => "50",
+    "name" => "Rubber banana 30 pcs.",
+    "material" => "Rubber",
+    "brand" => "Banana Republic",
+    "connection_type" => "",
+    "length" => "5cm",
+    "width" => "5cm",
+    "thickness" => ".3cm",
+    "type_id" => "9",
+    "image-input" => "",
+    "image-type" => "",
+    "old-image-path" => "",
+]));
 
 $task = "Update product with blank id";
-checkIfSuccess($task, $UpdateModel, "product", [
-    "" => "",
-]);
+checkIfSuccess($task, $UpdateModel, "product", json_encode([
+    "product-id" => "",
+    "unit-price" => "50",
+    "name" => "Rubber banana 30 pcs.",
+    "material" => "Rubber",
+    "brand" => "Banana Republic",
+    "connection_type" => "",
+    "length" => "5cm",
+    "width" => "5cm",
+    "thickness" => ".3cm",
+    "type_id" => "9",
+    "image-input" => "",
+    "image-type" => "",
+    "old-image-path" => "",
+]));
 
 $task = "Update product with null id";
-checkIfSuccess($task, $UpdateModel, "product", [
-    "" => "",
-]);
+checkIfSuccess($task, $UpdateModel, "product", json_encode([
+    "product-id" => null,
+    "unit-price" => "50",
+    "name" => "Rubber banana 30 pcs.",
+    "material" => "Rubber",
+    "brand" => "Banana Republic",
+    "connection_type" => "",
+    "length" => "5cm",
+    "width" => "5cm",
+    "thickness" => ".3cm",
+    "type_id" => "9",
+    "image-input" => "",
+    "image-type" => "",
+    "old-image-path" => "",
+]));
 
+$_SESSION["email-for-password-reset"] = "eneioarzew@gmail.com";
 $task = "Update password of existing user";
-checkIfSuccess($task, $UpdateModel, "password", "");
+checkIfSuccess($task, $UpdateModel, "password", "arcanE02!");
 
+$_SESSION["email-for-password-reset"] = "mark@mail.com";
 $task = "Update password of non-existent user";
-checkIfSuccess($task, $UpdateModel, "password", "");
+checkIfSuccess($task, $UpdateModel, "password", "arcanE02!");
 
-$task = "Update password of user with blank id";
-checkIfSuccess($task, $UpdateModel, "password", "");
+$_SESSION["email-for-password-reset"] = "";
+$task = "Update password of user with blank email";
+checkIfSuccess($task, $UpdateModel, "password", "arcanE02!");
 
-$task = "Update password of user with null id";
-checkIfSuccess($task, $UpdateModel, "password", "");
+$_SESSION["email-for-password-reset"] = null;
+$task = "Update password of user with null email";
+checkIfSuccess($task, $UpdateModel, "password", "arcanE02!");
